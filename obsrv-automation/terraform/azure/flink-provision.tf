9df725e1269aa76c137b704bc02b7c12c1e0c3a1
SadanandGowda <sadanand@sanketika.in>
2023-01-07 00:16:34 +0530

    Obsrv one click automation

---
+++ obsrv-automation/terraform/azure/flink-provision.tf
@@ -0,0 +1,26 @@
+resource "helm_release" "flink" {
+  name             = "druid-validator"
+  chart            = var.FLINK_CHART
+  namespace        = var.FLINK_NAMESPACE
+  create_namespace = true
+  depends_on       = [helm_release.kafka]
+  wait_for_jobs    = false
+  timeout = 900
+  
+  values = [
+    templatefile("../../helm_charts/flink/values.yaml",
+    {
+    }
+    )
+  ]
+}
+data "kubernetes_service" "flink" {
+  metadata {
+    namespace = "flink-dev"
+    name = "druid-validator-jobmanager"
+  }
+depends_on       = [azurerm_kubernetes_cluster.aks,helm_release.kafka]
+}
+
+
+
