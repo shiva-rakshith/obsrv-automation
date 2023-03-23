0998698889dd00689bb9c3b980cc7c3f8c3a6371
Manjunath Davanam <manjunath@sanketika.in>
2023-03-23 16:39:30 +0530

    #4 fix: Ingestion automation - removed the unwanted files and removed the commented code

--- terraform/aws/submit_ingestions.tf
+++ terraform/aws/submit_ingestions.tf
@@ -6,13 +6,4 @@ resource "helm_release" "submit-ingestions" {
     depends_on       = [helm_release.kafka, helm_release.druid_operator, helm_release.druid_cluster]
     wait_for_jobs    = var.submit_ingestion_wait_for_jobs
     timeout          = var.submit_ingestion_chart_install_timeout
-        
-    # values = [
-    #   templatefile(var.submit_ingestion_chart_custom_values_yaml,
-    #   {
-    #       checkpoint_store_type = var.submit_ingestion_namespace
-    #       s3_access_key         = coalesce(local.s3_access_key, "")
-    #       s3_secret_key         = coalesce(local.s3_secret_key, "")
-    #   })
-    # ]
 }
