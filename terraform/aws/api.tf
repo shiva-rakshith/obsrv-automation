163e1371d4bb8c619ce857a9fda40840e832dd12
ManojCKrishna <Manoj.Chintaluri@syngenta.com>
2023-03-28 19:32:39 +0530

    issue: Feat: Symlink for api

--- terraform/aws/api.tf
+++ terraform/aws/api.tf
@@ -1,15 +1,1 @@
-resource "helm_release" "api" {
-    name             = var.api_release_name
-    chart            = var.api_chart_path
-    namespace        = var.api_namespace
-    create_namespace = var.api_create_namespace
-    depends_on       = [helm_release.druid_operator, helm_release.druid_cluster]
-    values = [
-      templatefile(var.api_chart_template,
-        {
-          env        = var.env
-          api_namespace = var.api_namespace
-        }
-      )
-    ]
-}
+/Users/manoj/Documents/sunbird/oneclick/obsrv-automation/terraform/helm/api.tf
